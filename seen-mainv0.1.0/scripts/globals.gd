d6cc5d3d2a8d13034a243765d0d2abdec047fa1a
eepy-sleepy1234 <jsnibbelink@gmail.com>
2026-03-10 12:03:42 -0700

    progressed the story a bit

--- seen-mainv0.1.0/scripts/globals.gd
+++ seen-mainv0.1.0/scripts/globals.gd
@@ -1,4 +1,10 @@
 extends Node
 
+signal story_progress_changed(new_value)
 var listening = true
-var progress = 0
+var _story_progress = 0
+var story_progress: int:
+	get: return _story_progress
+	set(value):
+		_story_progress = value
+		story_progress_changed.emit(value)
--- seen-mainv0.1.0/scripts/labstory.gd
+++ seen-mainv0.1.0/scripts/labstory.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 @onready var objectives: Label = $CanvasLayer/objectivebox/HBoxContainer/objectives
+@onready var top: AnimatedSprite2D = $"tube top/AnimatedSprite2D"
+@onready var bottom: AnimatedSprite2D = $"tube bottom/AnimatedSprite2D"
+@onready var camera: Camera2D = $player/Camera2D
 
 var new_text = ""
+
+func wait_for_progress(target: int) -> void:
+	while Globals.story_progress != target:
+		await Globals.story_progress_changed
+
 func _ready() -> void:
 	await get_tree().create_timer(5).timeout
 	Globals.story_progress = 1
@@ -10,3 +18,10 @@ func _ready() -> void:
 	for i in new_text:
 		objectives.text += i
 		await get_tree().create_timer(0.01).timeout
+	await wait_for_progress(2)
+	await get_tree().create_timer(2).timeout
+	Globals.listening = true
+	Globals.story_progress = 3
+	top.play("oh no")
+	bottom.play("oh no")
+	camera.screen_shake(20, 1)
