293c53bd780dead6d57d179c519e33f01b2b6d1d
eepy-sleepy1234 <jsnibbelink@gmail.com>
2026-03-10 19:03:39 -0700

    basically done with the lab scene

--- seen-mainv0.1.0/scripts/labstory.gd
+++ seen-mainv0.1.0/scripts/labstory.gd
@@ -4,6 +4,10 @@ extends Node2D
 @onready var top: AnimatedSprite2D = $"tube top/AnimatedSprite2D"
 @onready var bottom: AnimatedSprite2D = $"tube bottom/AnimatedSprite2D"
 @onready var camera: Camera2D = $player/Camera2D
+@onready var red_flash: AnimationPlayer = $"CanvasLayer/red flash/AnimationPlayer"
+@onready var textbox: MarginContainer = $CanvasLayer/textbox
+@onready var player: CharacterBody2D = $player
+@onready var blackout: AnimationPlayer = $CanvasLayer/blackout/AnimationPlayer
 
 var new_text = ""
 
@@ -25,3 +29,45 @@ func _ready() -> void:
 	top.play("oh no")
 	bottom.play("oh no")
 	camera.screen_shake(20, 1)
+	Globals.listening = false
+	red_flash.play("red flash")
+	textbox.write_text("I need to shut this down")
+	await get_tree().create_timer(3).timeout
+	Globals.listening = true
+	new_text = "\n - push the emergency stop button"
+	for i in new_text:
+		objectives.text += i
+		await get_tree().create_timer(0.01).timeout
+	textbox.close_box()
+	await wait_for_progress(4)
+	red_flash.stop(false)
+	top.play("default")
+	bottom.play("default")
+	await get_tree().create_timer(2).timeout
+	textbox.write_text("phew, that was a close one")
+	await get_tree().create_timer(3).timeout
+	player.direction = Vector2(1, 0)
+	player.sprite.flip_h = true
+	player.sprite.play("side")
+	textbox.close_box()
+	await get_tree().create_timer(0.5).timeout
+	player.direction = Vector2(0, 1)
+	player.sprite.play("down")
+	await get_tree().create_timer(1.1).timeout
+	player.direction = Vector2(1, 0)
+	player.sprite.flip_h = true
+	player.sprite.play("side")
+	await get_tree().create_timer(0.5).timeout
+	player.direction = Vector2(-1, 0)
+	player.sprite.flip_h = false
+	player.direction = Vector2(0, 0)
+	player.last_direction = 1
+	await get_tree().create_timer(1).timeout
+	player.scale.y += 0.1
+	await get_tree().create_timer(0.1).timeout
+	player.scale.y -= 0.1
+	player.position = player.position.lerp(Vector2(player.position.x, player.position.y + 40), 0.2)
+	await get_tree().create_timer(0.5).timeout
+	camera.screen_shake(20, 10)
+	blackout.play("fade out")
+	
