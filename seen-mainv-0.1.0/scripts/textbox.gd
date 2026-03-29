286ad8d7da6be0d4bdf9760d24cbf1b59be8d4fe
Ausipainter <kjone795@evit.edu>
2026-03-28 19:29:38 -0700

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- seen-mainv-0.1.0/scripts/textbox.gd
+++ seen-mainv-0.1.0/scripts/textbox.gd
@@ -2,12 +2,17 @@ extends MarginContainer
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var textlabel: Label = $"../textbox/MarginContainer/HBoxContainer/Label"
+@onready var audio  = $"../../AudioStreamPlayer"
+@onready var audio2  =$"../../OldGuyWait"
+
 
 var open = false
 var tween = create_tween()
 
 func write_text(new_text) -> void:
 	Globals.listening = false
+	if new_text == "Hey there.":
+		audio.play()
 	textlabel.text = new_text
 	open_box()
 	if tween:
@@ -16,12 +21,15 @@ func write_text(new_text) -> void:
 	await get_tree().create_timer(0.5).timeout
 	tween = create_tween()
 	tween.tween_property(textlabel, "visible_ratio", 1, len(textlabel.text) * 0.05)
+
 	await tween.finished
 	if new_text not in ["I should give this to the ghost up there first", "phew, that was a close one", "don't walk out on her like that", "Why can I walk through everything besides these stairs?"]:
 		Globals.listening = true
 
 func continue_text(new_text) -> void:
 	Globals.listening = false
+	if new_text == "WAIT I NEED YOUR HELP":
+		audio2.play()
 	textlabel.text = new_text
 	if not open:
 		open_box()
@@ -34,6 +42,7 @@ func continue_text(new_text) -> void:
 	await tween.finished
 	if new_text not in ["you should get back in bed", "and why can I talk to people and ghosts?"]:
 		Globals.listening = true
+	
 
 func open_box():
 	open = true
