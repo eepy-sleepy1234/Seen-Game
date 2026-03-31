3f04707891a33b871edfc02df8255ae6ccd829c2
eepy-sleepy1234 <jsnibbelink@gmail.com>
2026-03-30 22:38:08 -0700

    more junk

--- seen-mainv-0.1.0/scripts/boss.gd
+++ seen-mainv-0.1.0/scripts/boss.gd
@@ -21,67 +21,90 @@ extends AnimatedSprite2D
 @onready var enemy19: enemy = $"../small/enemy19"
 @onready var enemy20: enemy = $"../small/enemy20"
 @onready var enemy21: enemy = $"../small/enemy21"
-var phase = 1
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	Globals.inventory = "2"
-	anim.play("float")
-	enemy18.go = false
-	enemy19.go = false
-	enemy20.go = false
-	enemy21.go = false
-	phase1()
-	await get_tree().create_timer(15).timeout
-	phase = 1.5
-	await get_tree().create_timer(8).timeout
-	anim.play("fall")
-	enemy18.anim.play("fade")
-	enemy19.anim.play("fade")
-	enemy20.anim.play("fade")
-	enemy21.anim.play("fade")
-	enemy18.go = false
-	enemy19.go = false
-	enemy20.go = false
-	enemy21.go = false
-	
-func phase1():
-	anim.play("attacking")
-	while phase == 1:
-		await get_tree().create_timer(1.4).timeout
-		enemy1.startup()
-		enemy2.startup()
-		enemy3.startup()
-		enemy4.startup()
-		await get_tree().create_timer(1.4).timeout
-		enemy5.startup()
-		enemy6.startup()
-		enemy7.startup()
-		enemy8.startup()
-		await get_tree().create_timer(1.4).timeout
-		enemy9.startup()
-		enemy10.startup()
-		enemy11.startup()
-		enemy12.startup()
-		await get_tree().create_timer(1.4).timeout
-		enemy13.startup()
-		enemy14.startup()
-		enemy15.startup()
-		enemy16.startup()
-		enemy17.startup()
-		enemy18.anim.play("fade")
-		enemy19.anim.play("fade")
-		enemy20.anim.play("fade")
-		enemy21.anim.play("fade")
-		enemy18.go = false
-		enemy19.go = false
-		enemy20.go = false
-		enemy21.go = false
-		await get_tree().create_timer(1.4).timeout
-		enemy18.startup()
-		enemy19.startup()
-		enemy20.startup()
-		enemy21.startup()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+enum Phase { PHASE1, PHASE2, PHASE3, RESTING, WIN }
+var current_phase: Phase = Phase.PHASE1
+var boss_was_hit := false
+
+const PHASE1_DURATION := 15.0
+const PHASE2_DURATION := 15.0
+const PHASE3_DURATION := 15.0
+const REST_WINDOW := 5
+
+func _ready() -> void:
+	Globals.puzzle = 11
+	Globals.inventory = "2"
+	run_fight()
+	
+func run_fight() -> void:
+	while current_phase != Phase.WIN:
+		match current_phase:
+			Phase.PHASE1:
+				await run_phase(PHASE1_DURATION, phase1_attack)
+			Phase.PHASE2:
+				await run_phase(PHASE2_DURATION, phase2_attack)
+			Phase.PHASE3:
+				await run_phase(PHASE3_DURATION, phase3_attack)
+
+func run_phase(duration: float, attack_func: Callable) -> void:
+	anim.play("attacking")
+	var timer := 0.0
+	
+	while timer < duration:
+		attack_func.call()
+		await get_tree().create_timer(1.4 * 5).timeout
+		timer += 1.4 * 5
+	await rest_window()
+
+func rest_window() -> void:
+	boss_was_hit = false
+	anim.play("fall")
+	hide_small_enemies()
+	var elapsed := 0.0
+	var interval := 0.1
+	
+	while elapsed < REST_WINDOW:
+		await get_tree().create_timer(interval).timeout
+		elapsed += interval
+		if boss_was_hit:
+			advance_phase()
+			return
+	anim.play("float")
+
+func advance_phase() -> void:
+	match current_phase:
+		Phase.PHASE1:
+			current_phase = Phase.PHASE2
+		Phase.PHASE2:
+			current_phase = Phase.PHASE3
+		Phase.PHASE3:
+			current_phase = Phase.WIN
+			win()
+
+func win():
+	pass
+
+func hide_small_enemies():
+	for e in [enemy18, enemy19, enemy20, enemy21]:
+		e.anim.play("fade")
+		e.go = false
+
+func phase1_attack():
+	await get_tree().create_timer(1.4).timeout
+	enemy1.startup(); enemy2.startup(); enemy3.startup(); enemy4.startup()
+	await get_tree().create_timer(1.4).timeout
+	enemy5.startup(); enemy6.startup(); enemy7.startup(); enemy8.startup()
+	await get_tree().create_timer(1.4).timeout
+	enemy9.startup(); enemy10.startup(); enemy11.startup(); enemy12.startup()
+	await get_tree().create_timer(1.4).timeout
+	enemy13.startup(); enemy14.startup(); enemy15.startup()
+	enemy16.startup(); enemy17.startup()
+	hide_small_enemies()
+	await get_tree().create_timer(1.4).timeout
+	enemy18.startup(); enemy19.startup(); enemy20.startup(); enemy21.startup()
+
+func phase2_attack():
+	pass
+	
+func phase3_attack():
 	pass
--- seen-mainv-0.1.0/scripts/throwbox.gd
+++ seen-mainv-0.1.0/scripts/throwbox.gd
@@ -1,7 +1,10 @@
 extends Sprite2D
 @onready var player: CharacterBody2D = %player
+@onready var boss_room: Node2D = $".."
+@onready var boss: AnimatedSprite2D = $"../boss"
 var direction = 0
-
+var distance = 0
+var itemScene = load("uid://dak20trdxfplt")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	visible = false
@@ -10,16 +13,36 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if direction == 1:
-		position.y -= 3
+		if position.y > -240:
+			position.y -= 3
+		distance -= 1
 	if direction == 2:
-		position.x += 3
+		if position.x < 145:
+			position.x += 3
+		distance -= 1
 	if direction == 3:
-		position.y += 3
+		if position.y < 0:
+			position.y += 3
+		distance -= 1
 	if direction == 4:
-		position.x -= 3
+		if position.x > -145:
+			position.x -= 3
+		distance -= 1
+	if distance == 0:
+		visible = false
+		var item_instance = itemScene.instantiate()
+		item_instance.item_type = "2"
+		item_instance.position = position
+		boss_room.add_child(item_instance)
 
 
 func _on_inventory_throw() -> void:
 	position = player.position
 	direction = player.last_direction
+	distance = 20
 	visible = true
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if fmod(boss.phase, 1.0) != 0.0:
+		direction = 3
+		distance = 15
